3e39fc58ad40671ef2dcb6b24604a4013979379f
afr1993 <31361041+afr1993@users.noreply.github.com>
2023-08-08 07:32:36 -0700

    toques finales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shares.module';
+import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { CardListComponent } from './components/card-list/card.list.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardComponent } from './components/card/card.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -10,11 +12,14 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
   declarations: [
   HomePageComponent,
   CardListComponent,
-  SearchBoxComponent
+  SearchBoxComponent,
+  CardComponent
 
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
+
   ],
   exports:[
     HomePageComponent
